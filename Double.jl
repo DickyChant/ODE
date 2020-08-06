e490b0d9b8ef270c75e6138df24bbc06819ef027
DickyChant <761337540@qq.com>
2020-08-06 12:18:52 +0800

    First Patch of DP

--- Double.jl
+++ Double.jl
@@ -11,15 +11,15 @@ l1 = 7.0
 m2 = 3.0
 l2 = 5.0
 
-function ddtheta1(theta1,theta2)
-    n = -3*(g*l1*m1*sin(theta1)+2*g*l1*m2*sin(theta1)-g*l2*m2*sin(theta2))
-    d = (2*l1^2*m1-3*l1*l2*m2+6*l2^2*m2)
+function ddtheta1(theta1,theta2,dtheta1,dtheta2)
+    n = -3*(3*cos(theta1-theta2)*dtheta1*dtheta2*l1^2*m2*sin(theta1-theta2)+2*dtheta1*dtheta2*l1*l2*m2*sin(theta1-theta2)+2*g*l1*m1*sin(theta1)+4*g*l1*m2*sin(theta1)-3*cos(theta1-theta2)*g*l1*m2*sin(theta2))
+    d = (4*l1^2*m1-9*cos(theta1-theta2)^2*l1^2*m2+12*l2^2*m2)
     return n/d
 end
 
-function ddtheta2(theta1,theta2)
-    n = -3*(-3*g*l1^2*m1*sin(theta1)-6*g*l1^2*m2*sin(theta1)+2*g*l1^2*m1*sin(theta2)+6*g*l2^2*m2*sin(theta2))
-    d = 2*l2*(2*l1^2*m1-3*l1*l2*m2+6*l2^2*m2)
+function ddtheta2(theta1,theta2,dtheta1,dtheta2)
+    n = 3*(2*dtheta1*dtheta2*l1^3*m1*sin(theta1-theta2)+3*cos(theta1-theta2)*dtheta1*dtheta2*l1^2*l2*m2*sin(theta1-theta2)+6*dtheta1*dtheta2*l1*l2^2*m2*sin(theta1-theta2)+3*cos(theta1-theta2)*g*l1^2*m1*sin(theta1)+6*cos(theta1-theta2)*g*l1^2*m2*sin(theta1)-2*g*l1^2*m1*sin(theta2)-6*g*l2^2*m2*sin(theta2))
+    d = l2*(2*l1^2*m1-3*l1*l2*m2+6*l2^2*m2)
     return n/d
 end
 
@@ -40,7 +40,7 @@ function y2(theta1,theta2)
 end
 
 function energy(theta1,theta2,dtheta1,dtheta2)
-    ret = (m1*l1^2/6+m2*l1^2/2)*dtheta1^2+m2*l2^2/6*dtheta2^2+m2*l1*l2*dtheta1*dtheta2/2-(m1/2+m2)*g*l1*cos(theta1)-m2/2*g*l2*cos(theta2)
+    ret = (m1*l1^2/6+m2*l1^2/2)*dtheta1^2+m2*l2^2/6*dtheta2^2+m2*l1*l2*dtheta1*dtheta2*cos(theta1-theta2)/2-(m1/2+m2)*g*l1*cos(theta1)-m2/2*g*l2*cos(theta2)
     return ret
 end
 
@@ -69,15 +69,15 @@ for i in 1:5000
 
     push!(theta1e,tmp1+dt*tmpd1)
     push!(theta2e,tmp2+dt*tmpd2)
-    push!(dtheta1e,tmpd1+dt*ddtheta1(tmp1,tmp2))
-    push!(dtheta2e,tmpd2+dt*ddtheta2(tmp1,tmp2))
+    push!(dtheta1e,tmpd1+dt*ddtheta1(tmp1,tmp2,tmpd1,tmpd2))
+    push!(dtheta2e,tmpd2+dt*ddtheta2(tmp1,tmp2,tmpd1,tmpd2))
     tmpt = ts[i] + dt
     push!(ts,tmpt)
 end
 
-plot(ts,[theta1e,theta2e],title="Double pendulum Euler, θ1 & θ2",label = ["θ1" "θ2"])
+plot(ts,[theta1e,theta2e],title="Double pendulum Euler, theta1 & theta2",label = ["theta1" "theta2"])
 savefig("dpe.pdf")
-plot(ts,[dtheta1e,dtheta2e],title="Double pendulum Euler, θ1_dot & θ2_dot",label = ["θ1_dot" "θ2_dot"])
+plot(ts,[dtheta1e,dtheta2e],title="Double pendulum Euler, theta1_dot & theta2_dot",label = ["theta1_dot" "theta2_dot"])
 savefig("dped.pdf")
 
 # Frogleap
@@ -94,14 +94,14 @@ for i in 1:2500
     tmpd1 = dtheta1f[i]
     tmpd2 = dtheta2f[i]
 
-    halfd1 = tmpd1 + dt*ddtheta1(tmp1,tmp2)
-    halfd2 = tmpd2 + dt*ddtheta2(tmp1,tmp2)
+    halfd1 = tmpd1 + dt*ddtheta1(tmp1,tmp2,tmpd1,tmpd2)
+    halfd2 = tmpd2 + dt*ddtheta2(tmp1,tmp2,tmpd1,tmpd2)
     new1 = tmp1+2*dt*halfd1
     new2 = tmp2+2*dt*halfd2
     push!(theta1f,new1)
     push!(theta2f,new2)
-    push!(dtheta1f,halfd1+ddtheta1(new1,new2)*dt)
-    push!(dtheta2f,halfd2+ddtheta2(new1,new2)*dt)
+    push!(dtheta1f,halfd1+ddtheta1(new1,new2,halfd1,halfd2)*dt)
+    push!(dtheta2f,halfd2+ddtheta2(new1,new2,halfd1,halfd2)*dt)
 end
 
 tsprime = [(ts[2*i-1]) for i in 1:2501]
--- Double.md
+++ Double.md
@@ -1,6 +1,6 @@
 # Lagrangian
 $$
-T = \frac{1}{2}m_1(l_1\dot{\theta}_1)^2 + \frac{1}{2}\frac{1}{12}m_1l_1^2\dot{\theta}_1^2+\frac{1}{2}m_2(\frac{l_2\dot{\theta}_2}{2}+l_1\dot{\theta}_1)^2 + \frac{1}{2}\frac{1}{12}m_2l_2^2\dot{\theta}_2^2
+T = \frac{1}{2}m_1(l_1\dot{\theta}_1)^2 + \frac{1}{2}\frac{1}{12}m_1l_1^2\dot{\theta}_1^2+\frac{1}{2}m_2(\frac{l_2^2\dot{\theta}_2^2}{4}+l_1^2\dot{\theta}_1^2+l_1\dot{\theta}_1l_2\dot{\theta}_2\cos(\theta_1-\theta_2)) + \frac{1}{2}\frac{1}{12}m_2l_2^2\dot{\theta}_2^2
 $$
 
 $$
@@ -12,11 +12,11 @@ L = T-V
 $$
 
 $$
-(\frac{1}{3}m_1l_1^2+m_2l_2^2)\ddot{\theta}_1+(\frac{1}{2}m_2l_1l_2)\ddot{\theta}_2=-(\frac{m_1}{2}+m_2)gl_1\sin{\theta}_1
+(\frac{1}{3}m_1l_1^2+m_2l_2^2)\ddot{\theta}_1+(\frac{1}{2}m_2l_1l_2\cos(\theta_1-\theta_2))\ddot{\theta}_2\\=-(\frac{m_1}{2}+m_2)gl_1\sin{\theta}_1-\frac{1}{2}m_2l_1l_2\dot{\theta}_1\dot{\theta}_2\sin(\theta_1-\theta_2)
 $$
 
 $$
-(\frac{1}{2}m_2l_1l_2)\ddot{\theta}_1+(\frac{1}{3}m_2l_2^2)\ddot{\theta}_2=-(\frac{m_2}{2})gl_2\sin{\theta_2}
+(\frac{1}{2}m_2l_1l_2\cos(\theta_1-\theta_2))\ddot{\theta}_1+(\frac{1}{3}m_2l_2^2)\ddot{\theta}_2=-(\frac{m_2}{2})gl_2\sin{\theta_2}+\frac{1}{2}m_2l_1l_2\dot{\theta}_1\dot{\theta}_2\sin(\theta_1-\theta_2)
 $$
 
 $$
